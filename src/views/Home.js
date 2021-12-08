823f8c4b4cc28295760a5f388c626aaf18542e4e
GRANT NATIONS <u1247784@umail.utah.edu>
2021-12-07 17:14:39 -0700

    add Home view and misc folders to src

---
+++ src/views/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../css/Home.css"
+
+/**
+ * The home page view for the pupil landing site.
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function Home(){
+    return <div className={"home-page"}>
+        <div className={"main-header"}>
+            <h1>
+                pupil
+            </h1>
+        </div>
+
+    </div>
+}
+
+export default Home;
